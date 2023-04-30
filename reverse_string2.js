e7e56847a2bf0ef02e9d86b899ea8fd9fab46004
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-30 12:50:50 +0600

    reverse string in javaScript input name

---
+++ reverse_string2.js
@@ -0,0 +1,21 @@
+let fname = prompt("Enter first name: ");
+let lname = prompt("Enter last name: ");
+document.write(lname + ' '+ fname);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const name = ["Zunaeed", "Imran"]
+// document.getElementById("demo").innerHTML = name.reverse();
